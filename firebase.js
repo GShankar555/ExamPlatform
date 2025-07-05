8c56e86da441aeff92f02eac17c6b20e8381a4e7
Gowri Shankar <shankarseshamsetti@gmail.com>
2025-07-05 12:04:10 +0530

    env change

--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
+import dotenv from "dotenv";
+dotenv.config();
 
-const serviceAccount = JSON.parse(
-  readFileSync("./serviceAccountKey.json", "utf8")
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG_JSON);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
